37d9492442d904488a3f1285c62775de6abd5964
Petro Bahrynovskyi <pbahrynovskyi@gmail.com>
2025-09-08 11:07:59 +0300

    changed some logic

--- src/pages/TabContent.tsx
+++ src/pages/TabContent.tsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
-import { Tab } from '../types/Tab';
 
-const tabsData: Tab[] = [
+const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
@@ -10,7 +9,7 @@ const tabsData: Tab[] = [
 const TabContent: React.FC = () => {
   const { tabId } = useParams();
 
-  const currentTab = tabsData.find(tab => tab.id === tabId);
+  const currentTab = tabs.find(tab => tab.id === tabId);
 
   return (
     <div className="block" data-cy="TabContent">
--- src/pages/TabsPage.tsx
+++ src/pages/TabsPage.tsx
@@ -1,7 +1,8 @@
 import { Link, Outlet, useParams } from 'react-router-dom';
-import { Tab } from '../types/Tab';
 
-const tabsData: Tab[] = [
+import classNames from 'classnames';
+
+const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
@@ -15,17 +16,18 @@ export const TabsPage = () => {
       <h1 className="title">Tabs page</h1>
       <div className="tabs is-boxed">
         <ul>
-          {tabsData.map(tab => (
+          {tabs.map(tab => (
             <li
               data-cy="Tab"
               key={tab.id}
-              className={tabId === tab.id ? 'is-active' : ''}
+              className={classNames({ 'is-active': tabId === tab.id })}
             >
-              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+              <Link to={tab.id}>{tab.title}</Link>
             </li>
           ))}
         </ul>
       </div>
+
       <Outlet />
     </>
   );
